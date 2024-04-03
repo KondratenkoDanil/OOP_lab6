880ea184156a450c86d1ab39bb9896843c471974
KondratenkoDanil <129153766+KondratenkoDanil@users.noreply.github.com>
2024-04-03 23:38:02 +0300

    Create lab6.cs

---
+++ lab6_oop/lab6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static int ComputeIntegerPart(int numerator, int denominator)
+    {
+        if (denominator == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+
+        return numerator / denominator;
+    }
+
+    static void Main(string[] args)
+    {
+        int numerator = 17;
+        int denominator = 5;
+
+        int integerPart = ComputeIntegerPart(numerator, denominator);
+        Console.WriteLine($"The integer part of {numerator}/{denominator} is: {integerPart}");
+    }
+}
